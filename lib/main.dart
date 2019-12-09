a6d4fbefa759478efeb04b36791985aed5a9bde1
madhank93 <maddymadhan946@gmail.com>
2019-12-09 22:33:07 +0530

    Initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,114 +1,53 @@
-import 'package:flutter/material.dart';
-import 'dart:math' as math;
+import 'package:dotp/dotp.dart';
+import 'package:requests/requests.dart';
+import 'package:intl/intl.dart';
+import 'dart:convert';
+import "dart:io";
 
-void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
+void main() async {
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
+  // Loading config file
+  final file = new File('lib/config.json');
+  String content = file.readAsStringSync(encoding: Encoding.getByName("utf8"));
+  Map<String,dynamic> config = json.decode(content);
 
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
-  final String title;
+  // Login
+  var loginURL = 'https://hrvine.sysvine.com/index.php/index/loginpopupsave/?username=SYS1310&password=san@2018';
+  await Requests.get(loginURL);
 
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
+  // Handling 2-factor authentication
+  TOTP totp = TOTP("6EMLBKVQMDBJDAX3");
+  var otpURL = 'https://hrvine.sysvine.com/index.php/index/?verfication_code=${totp.now()}&user_id=lzpZSd';
+  await Requests.get(otpURL);
 
-class _MyHomePageState extends State<MyHomePage> {
-  //int _counter = 0;
-  var colors;
+  // Formatting current date
+  var now = new DateTime.now();
+  DateFormat formatter = new DateFormat('dd-MMM-yyyy');
+  String formatted = formatter.format(now);
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      colors = Color((math.Random().nextDouble() * 0xFFFFFF).toInt() << 0).withOpacity(1.0);
-    });
+  // Fetching time-entry records
+  var timesheetURL = 'https://hrvine.sysvine.com/index.php/timemanagement/attendance/getemployeeattendance/format/json/';
+  var timesheet = await Requests.post(timesheetURL, body:{"userId":199,"selectedDate": "$formatted"},bodyEncoding: RequestBodyEncoding.FormURLEncoded);
+
+  // Parsing time-entries
+  dynamic json = timesheet.json();
+  var totalHours = DateTime.parse("2019-12-06 00:00:00");
+  var requriedHours = DateTime.parse('2019-12-06 08:00:00');
+
+  for (var time in json) {
+      if (time['punch_in_user_time'] != null && time['punch_out_user_time'] != null) {
+        var inTime = DateTime.parse(time['punch_in_user_time']);
+        var outTime = DateTime.parse(time['punch_out_user_time']);
+        print(inTime);
+        print(outTime);
+        totalHours = (totalHours.add(outTime.difference(inTime)));
+      }
   }
 
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-      child:Container(
-        color: colors,
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
+  //print(totalHours < requriedHours);
 
-          children: <Widget>[
-            Text(
-              'Click on the button to change background color',
-            ),
-          ],
-        ),
-    )
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Change color',
-        child: Icon(Icons.add),
-        //backgroundColor: colors,
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
+  print(totalHours.toString().split(' ')[1]);
 }
