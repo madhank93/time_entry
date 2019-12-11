8bdd808cc12f78bf1afaec6e426be1fcaab37f0f
madhank93 <maddymadhan946@gmail.com>
2019-12-11 23:46:51 +0530

    added condition to check total working hour

--- lib/main.dart
+++ lib/main.dart
@@ -3,14 +3,15 @@ import 'package:requests/requests.dart';
 import 'package:intl/intl.dart';
 
 void main() async {
-
   // Login
-  var loginURL = 'https://hrvine.sysvine.com/index.php/index/loginpopupsave/?username=SYS1310&password=san@2018';
+  var loginURL =
+      'https://hrvine.sysvine.com/index.php/index/loginpopupsave/?username=SYS1310&password=san@2018';
   await Requests.get(loginURL);
 
   // Handling 2-factor authentication
   TOTP totp = TOTP("6EMLBKVQMDBJDAX3");
-  var otpURL = 'https://hrvine.sysvine.com/index.php/index/?verfication_code=${totp.now()}&user_id=lzpZSd';
+  var otpURL =
+      'https://hrvine.sysvine.com/index.php/index/?verfication_code=${totp.now()}&user_id=lzpZSd';
   await Requests.get(otpURL);
 
   // Formatting current date
@@ -19,25 +20,37 @@ void main() async {
   String formatted = formatter.format(now);
 
   // Fetching time-entry records
-  var timesheetURL = 'https://hrvine.sysvine.com/index.php/timemanagement/attendance/getemployeeattendance/format/json/';
-  var timesheet = await Requests.post(timesheetURL, body:{"userId":199,"selectedDate": "$formatted"},bodyEncoding: RequestBodyEncoding.FormURLEncoded);
+  var timesheetURL =
+      'https://hrvine.sysvine.com/index.php/timemanagement/attendance/getemployeeattendance/format/json/';
+  var timesheet = await Requests.post(timesheetURL,
+      body: {"userId": 199, "selectedDate": "$formatted"},
+      bodyEncoding: RequestBodyEncoding.FormURLEncoded);
 
   // Parsing time-entries
   dynamic json = timesheet.json();
   var totalHours = DateTime.parse("2019-12-06 00:00:00");
-  var requriedHours = DateTime.parse('2019-12-06 08:00:00');
+  var requiredHours = DateTime.parse("2019-12-06 08:00:00");
 
+  // Looping every punch in/out and calculating total worked hours
   for (var time in json) {
-      if (time['punch_in_user_time'] != null && time['punch_out_user_time'] != null) {
-        var inTime = DateTime.parse(time['punch_in_user_time']);
-        var outTime = DateTime.parse(time['punch_out_user_time']);
-        print(inTime);
-        print(outTime);
-        totalHours = (totalHours.add(outTime.difference(inTime)));
-      }
+    var outTime, inTime;
+    inTime = DateTime.parse(time['punch_in_user_time']);
+    if (time['punch_out_user_time'] != null) {
+      outTime = DateTime.parse(time['punch_out_user_time']);
+    } else {
+      outTime = DateTime.parse(DateTime.now().toString());
+    }
+    totalHours = (totalHours.add(outTime.difference(inTime)));
   }
 
-  //print(totalHours < requriedHours);
+  print(totalHours);
+  print(totalHours.hour);
 
-  print(totalHours.toString().split(' ')[1]);
+  // Checking required work hours
+  if (totalHours.hour >= 8) {
+    print("You have worked more than 8 hours => ${totalHours.toString().split(' ')[1]}");
+  } else {
+    var shortageHours = requiredHours.difference(totalHours);
+    print("You need to clock  $shortageHours");
+  }
 }
